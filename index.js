b5a944631ef0ce6635dec4ae484336b9e1ad8d28
michael.goeke <michael.goeke@dnvgl.com>
2021-06-08 09:58:33 -0700

    move phoneNumber out from post into USER_DETAILS

--- index.js
+++ index.js
@@ -2,31 +2,34 @@ const port = process.env.PORT || 3000;
 const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
-const basicAuthCredentials = JSON.parse(process.env.BASIC_AUTH_CREDENTIALS);
-
+const userDetails = JSON.parse(process.env.USER_DETAILS);
 const twilio = require('twilio')(twilioAccountSid, twilioAuthToken);
 const express = require('express');
 const basicAuth = require('express-basic-auth');
 
 const app = express();
 app.use(express.json());
+
+const basicAuthCredentials = {};
+for (const u in userDetails) basicAuthCredentials[u] = userDetails[u].password;
 app.use(basicAuth({ users: basicAuthCredentials }));
 
 const jobTimeouts = {};
 
-app.post('/', requireBody({jobName:"xxxService", phoneNumber:"555-123-4567", timeoutMinutes: 5, message: "service xxx is down"}), (req, res) => {
-  const { jobName, phoneNumber, timeoutMinutes, message } = req.body;
+app.post('/', requireBody({jobName:"xxxService", timeoutMinutes: 5, message: "service xxx is down"}), (req, res) => {
+  const { jobName, timeoutMinutes, message } = req.body;
   const jobId = `${req.auth.user}${jobName}`;
+  const phoneNumber = userDetails[req.auth.user].phoneNumber;
   jobTimeouts[jobId] && clearTimeout(jobTimeouts[jobId]);
-  const tim = setTimeout(() => {
-      sendMessage(message, phoneNumber)
+  const t = setTimeout(() => {
+    sendMessage(message, phoneNumber)
         .then(result => {
           delete jobTimeouts[jobId];
           if (result.status != 'accepted')
             console.log({result});
         });
     }, timeoutMinutes * 60 * 1000);
-    jobTimeouts[jobId] = tim;
+    jobTimeouts[jobId] = t;
   return res.status(200).send();
 });
 
@@ -53,8 +56,9 @@ function requireBody(expected) {
  }
 }
 
-//function sendMessage(body, to) { return new Promise(() => console.log(`<sms> ${to} ${body}`)); }
-function sendMessage(body, to) { return twilio.messages.create({ messagingServiceSid, body, to }); }
+const sendMessage = (app.get('env') === 'development')
+  ? (body, to) => new Promise(() => console.log(`<sms> ${to} ${body}`))
+  : (body, to) => twilio.messages.create({ messagingServiceSid, body, to });
 
 function getTimeLeft(timeout) {
   const milliseconds = timeout._idleStart + timeout._idleTimeout - (process.uptime() * 1000);
