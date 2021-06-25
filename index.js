7c9d153a7926405f613c91979f5616973ccb9fc4
michael.goeke <michael.goeke@dnvgl.com>
2021-06-25 09:14:52 -0700

    refactor api calls into /api (maintain post / for backwards compatibility with clients)

--- index.js
+++ index.js
@@ -20,7 +20,10 @@ app.use(basicAuth({ challenge: true, users: basicAuthCredentials }));
 
 const jobTimeouts = {};
 
-app.post('/', requireBody({jobName:"xxxService", timeoutMinutes: 5, message: "service xxx is down"}), (req, res) => {
+app.post('/api', requireBody({jobName:"xxxService", timeoutMinutes: 5, message: "service xxx is down"}), postJob);
+app.post('/', requireBody({jobName:"xxxService", timeoutMinutes: 5, message: "service xxx is down"}), postJob); // maintain old address until clients have switched over
+
+function postJob(req, res) {
   const { jobName, timeoutMinutes, message, initialMessage, meta } = req.body;
   const jobId = `${req.auth.user}${jobName}`;
   const phoneNumber = userDetails[req.auth.user].phoneNumber;
@@ -40,22 +43,32 @@ app.post('/', requireBody({jobName:"xxxService", timeoutMinutes: 5, message: "se
     }, timeoutMinutes * 60 * 1000);
   jobTimeouts[jobId] = {timeout: t, timeoutMinutes, meta};
   return res.status(200).send();
-});
+}
+
+app.get('/api', (req, res) => {
+  const user = req.auth.user;
+  res.send(getJobsDataFor(user));
+})
 
 
-const initTemplate = Handlebars.compile(fs.readFileSync('./index.handlebars', 'utf8'));
+var template = Handlebars.compile(fs.readFileSync('./index.handlebars', 'utf8'));
 app.get('/', (req, res) => {
   const user = req.auth.user;
-  var userJobs = user === 'admin' ? Object.keys(jobTimeouts) : Object.keys(jobTimeouts).filter(x => x.startsWith(user));
-  if (user === 'admin') user = '00000000-0000-0000-0000-00000000'; //retain just the last 4 digits for uniqueness in troubleshooting
-  var templateData = new Object();
-  userJobs.forEach(x => templateData[x.slice(user.length)] = { timeout: getSecondsLeft(jobTimeouts[x].timeout), timeoutPercent: getSecondsLeft(jobTimeouts[x].timeout) / (jobTimeouts[x].timeoutMinutes * 60) * 100, meta: jobTimeouts[x].meta });
-
-  const template = (process.env.NODE_ENV === 'production') ? initTemplate : Handlebars.compile(fs.readFileSync('./index.handlebars', 'utf8'));
+  var templateData = getJobsDataFor(user);
+  
+  if (process.env.NODE_ENV !== 'production') template = Handlebars.compile(fs.readFileSync('./index.handlebars', 'utf8'));
 
   const html = template(templateData);
   return res.send(html);
 });
+
+function getJobsDataFor(user) {
+  var userJobs = user === 'admin' ? Object.keys(jobTimeouts) : Object.keys(jobTimeouts).filter(x => x.startsWith(user));
+  if (user === 'admin') user = '00000000-0000-0000-0000-00000000'; //retain just the last 4 digits for uniqueness in troubleshooting
+  var templateData = new Object();
+  userJobs.forEach(x => templateData[x.slice(user.length)] = { timeout: getSecondsLeft(jobTimeouts[x].timeout), timeoutPercent: getSecondsLeft(jobTimeouts[x].timeout) / (jobTimeouts[x].timeoutMinutes * 60) * 100, meta: jobTimeouts[x].meta });
+  return templateData;
+}
 
 app.listen(port);
 
